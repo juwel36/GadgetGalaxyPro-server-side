06e4bf277fd0d3f1305173c389ffd2b2baa4ff03
juwel36 <nothingsomthing3@gmail.com>
2023-10-17 22:40:26 +0600

    take data from clint side and read also keep in database

--- index.js
+++ index.js
@@ -26,12 +26,27 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const gadgetcollection =client.db("GadgetDB").collection("products")
 
 
 
+    // read
+app.get('/products',async(req,res)=>{
+  const cursor= gadgetcollection.find()
+  const result= await cursor.toArray()
+  res.send(result)
+  })
+
+// create
+app.post('/products',async(req,res)=>{
+  const user=req.body;
+  const result = await gadgetcollection.insertOne(user);
+  res.send(result)
+  
+  })
 
 
-    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
